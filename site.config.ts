ea151f1c71a4fb35a0ddc56ef9fcb2df68c8e39d
wuzhouchao <62629899+wuzhouchao@users.noreply.github.com>
2023-02-20 12:01:55 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '吴超',
+  domain: 'chaower.com',
+  author: '吴超',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
